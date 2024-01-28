9fe2cbb73f924eec332b38d5d185894ec2cbf66f
Dmitry <example@gmail.com>
2024-01-28 21:17:05 +0300

    code refactoring

--- game-store-frontend/src/App.jsx
+++ game-store-frontend/src/App.jsx
@@ -1,13 +1,8 @@
 import { useState } from 'react'
-import Header from './components/elements/Header/Header'
-import Footer from './components/elements/Footer/Footer'
-<<<<<<< HEAD
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router'
-=======
-import { Provider } from 'react-redux'
-import { store } from './redux'
->>>>>>> e3a9074f7b2544254142ab089da44b4be6aef7b0
+
+
 
 function App() {
   const [count, setCount] = useState(0)
--- game-store-frontend/src/main.jsx
+++ game-store-frontend/src/main.jsx
@@ -6,13 +6,7 @@ import { Provider } from 'react-redux'
 import { store } from './redux/index.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-<<<<<<< HEAD
-    <App />
-=======
-  <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
->>>>>>> e3a9074f7b2544254142ab089da44b4be6aef7b0
 )
