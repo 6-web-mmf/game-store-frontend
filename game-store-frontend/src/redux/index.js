283ab2ecb34c0686f7330b0604ad65728ff5ed79
Artem-Elyashevich <elyashevich.artem06@gmail.com>
2024-01-31 22:10:04 +0300

    add: cart slice

--- game-store-frontend/src/redux/index.js
+++ game-store-frontend/src/redux/index.js
@@ -3,15 +3,17 @@ import gameSlice from './slices/gameSlice'
 import commentSlice from './slices/commentSlice'
 import { rememberReducer, rememberEnhancer } from "redux-remember"
 import categorySlice from './slices/categorySlice'
+import cartSlice from './slices/cartSlice'
 
 
-const rememberedKeys = ['category', 'game', 'comment']
+const rememberedKeys = ['category', 'game', 'comment', 'cart']
 
 export const store = configureStore({
     reducer: rememberReducer({
         game: gameSlice,
         comment: commentSlice,
-        category: categorySlice
+        category: categorySlice,
+        cart: cartSlice
     }),
     enhancers: getDefaultEnhancers => getDefaultEnhancers().concat(
         rememberEnhancer(
